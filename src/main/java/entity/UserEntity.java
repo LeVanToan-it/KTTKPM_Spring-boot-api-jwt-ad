0fdeb5991c53b5de1a123aa063cbdf0f6d66abe6
Le Van Toan <lvtoan.it2000@gmail.com>
2021-09-22 12:03:08 +0700

    add security maven

--- src/main/java/entity/UserEntity.java
+++ src/main/java/entity/UserEntity.java
@@ -1,5 +1,26 @@
 package entity;
 
-public class UserEntity {
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name="t_user")
+@Getter
+@Setter
+public class UserEntity extends BaseEntity {
+	private String username;
+	private String password;
+	
+	@OneToMany
+	@JoinTable(name="t_user_role", joinColumns= {@JoinColumn(name="user_id")},inverseJoinColumns= {@JoinColumn(name="role_id")})
+	private Set<RoleEntity> roles = new HashSet<>();
 }
